8332415981ddc2b96a0df9ef2205de59fa432552
lucasmoutinho2503 <moutinho319@gmail.com>
2025-06-20 09:01:37 -0300

    first commit

---
+++ src/main/java/com/example/api_literalura/ApiLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLiteraluraApplication.class, args);
+	}
+
+}
